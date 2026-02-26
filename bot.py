7a1295ef7ef5126e2d0cd6f51b27e13c42c5b764
loginovvvladislav-create <loginovvvladislav@gmail.com>
2026-02-26 17:21:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,45 @@
-from telegram.ext import Updater, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
 TOKEN = "8793875356:AAHq6CqTB5TpBpR_dYWmlc8d86fHZP5vR_A"
 
 def start(update, context):
+    user_id = update.effective_user.id
+
+    keyboard = [
+        [InlineKeyboardButton("🔗 Получить ссылку", callback_data="get_link")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
     update.message.reply_text(
         "👁 Анонимная правда о тебе\n\n"
         "Я помогу узнать, что о тебе думают на самом деле.\n"
         "Анонимно. Бесплатно. Без имён.\n\n"
-        "👇 Нажми кнопку ниже"
+        "👇 Нажми кнопку ниже",
+        reply_markup=reply_markup
     )
+
+def button_handler(update, context):
+    query = update.callback_query
+    query.answer()
+
+    if query.data == "get_link":
+        user_id = query.from_user.id
+        bot_username = context.bot.username
+        link = f"https://t.me/{bot_username}?start=anon_{user_id}"
+
+        query.edit_message_text(
+            f"🔗 Твоя личная ссылка:\n\n"
+            f"{link}\n\n"
+            "Отправь её друзьям.\n"
+            "Они смогут написать о тебе честно и анонимно."
+        )
 
 updater = Updater(TOKEN)
 dp = updater.dispatcher
+
 dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CallbackQueryHandler(button_handler))
 
 updater.start_polling()
 updater.idle()
